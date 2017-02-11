1e4df1e44fcd0dccc0541cab80c2d48e95e844e8
Young <ylee546@wisc.edu>
2017-02-11 16:14:10 -0600

    ETL: Modify clean2.py to write in utf8. Remove 2nd line in film.csv.

--- ETL/clean2.py
+++ ETL/clean2.py
@@ -1,31 +1,31 @@
 import csv
 inputfile = open('film.csv',encoding='mac_roman')
-outputfile = open('film_clean.csv','w',encoding='mac_roman')
+outputfile = open('film_clean.csv','w',encoding='utf-8')
 reader = csv.DictReader(inputfile,delimiter=';',skipinitialspace=True)
 header = reader.fieldnames
 
 # clean header
 for i in range(len(header)):
-	header[i] = header[i].lower().strip()
+        header[i] = header[i].lower().strip()
 
 
 writer = csv.DictWriter(outputfile,delimiter=',',fieldnames=header)
 writer.writeheader()
 
+line_no = 0
 for row in reader:
-
-	for key, value in row.items():
-		row[key] = row[key].lower()
-		row[key] = row[key].strip()
-		row[key].replace(',','')
-
-		if row[key] == '':
-			row[key] = '.'
-
-		if key == 'actor' or key == 'actress' or key == 'director':
-			row[key] = row[key].replace(',','')
-
-	writer.writerow(row)
+        line_no += 1
+        if line_no == 1:
+                continue
+        for key, value in row.items():
+                row[key] = row[key].lower()
+                row[key] = row[key].strip()
+                row[key].replace(',', '')
+                if row[key] == '':
+                        row[key] = '.'
+                if key == 'actor' or key == 'actress' or key == 'director':
+                        row[key] = row[key].replace(',','')
+        writer.writerow(row)
 
 
 inputfile.close()
